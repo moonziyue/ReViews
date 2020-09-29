f65934baa7c4e7e69dac1c3937aaa8d2afae8524
moonziyue <moonlesswylb@gmail.com>
2020-09-29 14:30:20 -0400

    Update clean_ucsd_data.py

--- data-processing/clean_ucsd_data.py
+++ data-processing/clean_ucsd_data.py
@@ -15,6 +15,12 @@ filtered_meta_df=filtered_meta_df.dropDuplicates(['asin'])
 filtered_meta_df=filtered_meta_df.withColumnRenamed('asin','product_id')
 filtered_meta_df=filtered_meta_df.withColumnRenamed('main_cat','product_category')
 filtered_meta_df=filtered_meta_df.withColumnRenamed('title','product_title')
+
+# filter unformatted rows, i.e. some may still contain html style content
+filtered_meta_df=filtered_meta_df.filter(~filtered_meta_df.product_id.contains('getTime()'))
+filtered_meta_df=filtered_meta_df.filter(~filtered_meta_df.product_category.contains('getTime()'))
+filtered_meta_df=filtered_meta_df.filter(~filtered_meta_df.product_title.contains('getTime()'))
+
 # filtered_meta_df.printSchema()
 
 # REVIEW_DATA
@@ -29,6 +35,7 @@ ucsd_review_df.printSchema()
 
 # rename columns
 ucsd_review_df=ucsd_review_df.withColumnRenamed('asin','product_id')
+ucsd_review_df=ucsd_review_df.withColumnRenamed('overall','star_rating')
 ucsd_review_df=ucsd_review_df.withColumnRenamed('reviewText','review_body')
 ucsd_review_df=ucsd_review_df.withColumnRenamed('reviewerID','customer_id')
 ucsd_review_df=ucsd_review_df.withColumnRenamed('summary','review_headline')
@@ -36,8 +43,9 @@ ucsd_review_df=ucsd_review_df.withColumnRenamed('verified','verified_purchase')
 ucsd_review_df=ucsd_review_df.withColumnRenamed('vote','helpful_votes')
 
 # convert dtype
-ucsd_review_df=ucsd_review_df.withColumn('star_rating', ucsd_review_df.overall.cast('INT'))
-ucsd_review_df=ucsd_review_df.drop('overall')
+ucsd_review_df=ucsd_review_df.withColumn('star_rating', ucsd_review_df.star_rating.cast('INT'))
+ucsd_review_df=ucsd_review_df.withColumn('helpful_votes', ucsd_review_df.helpful_votes.cast('INT'))
+
 # unixtime -> date
 ucsd_review_df=ucsd_review_df.withColumn('review_date', from_unixtime('unixReviewTime',format='yyyy-MM-dd'))
 ucsd_review_df=ucsd_review_df.withColumn('review_date', to_date('review_date',format='yyyy-MM-dd'))
@@ -46,7 +54,16 @@ ucsd_review_df=ucsd_review_df.drop('unixReviewTime')
 # drop NA
 ucsd_review_df=ucsd_review_df.dropna(how='any', subset=['customer_id','product_id','star_rating','review_date'])
 
+# filter unformatted rows, i.e. some may still contain html style content
+ucsd_review_df=ucsd_review_df.filter(~ucsd_review_df.product_id.contains('getTime()'))
+ucsd_review_df=ucsd_review_df.filter(~ucsd_review_df.review_body.contains('getTime()'))
+ucsd_review_df=ucsd_review_df.filter(~ucsd_review_df.customer_id.contains('getTime()'))
+ucsd_review_df=ucsd_review_df.filter(~ucsd_review_df.review_headline.contains('getTime()'))
+
 # ucsd_review_df.printSchema()
+
+# JOIN
+ucsd_df=ucsd_review_df.join(filtered_meta_df, 'product_id', 'inner')
 
 
 
