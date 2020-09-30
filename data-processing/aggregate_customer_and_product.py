d1f99336e0fe45ed6c98c749e5a7c715478b67e1
moonziyue <moonlesswylb@gmail.com>
2020-09-29 22:32:28 -0400

    Create aggregate_customer_and_product.py

---
+++ data-processing/aggregate_customer_and_product.py
@@ -0,0 +1,26 @@
+from pyspark.sql.functions import *
+
+def aggregate_customer(df):
+	# agg star_rating
+	customer_rating_df=df.groupby('customer_id').pivot('star_rating').count().fillna(0)
+
+	customer_rating_df=customer_rating_df.withColumnRenamed('1','one_star')
+	customer_rating_df=customer_rating_df.withColumnRenamed('2','two_star')
+	customer_rating_df=customer_rating_df.withColumnRenamed('3','three_star')
+	customer_rating_df=customer_rating_df.withColumnRenamed('4','four_star')
+	customer_rating_df=customer_rating_df.withColumnRenamed('5','five_star')
+	# customer_rating_df.show()
+
+	# agg other info
+	stats_df=df.groupBy('customer_id').agg(
+    count('review_id').alias('count'),
+    sum(when(col('verified_purchase') == True, 1)).alias('verified_purchase'),
+    sum('helpful_votes').alias('helpful_votes')
+	)
+	# stats_df.show()
+
+	# join
+	customer_summary_df=customer_rating_df.join(stats_df, 'customer_id', 'inner')
+	# customer_summary_df.show()
+
+	return customer_summary_df
