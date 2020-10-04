5f09ddbab524c89df512ba9a581e13475945c9e2
moonziyue <moonlesswylb@gmail.com>
2020-10-04 12:03:37 -0400

    Update schema.sql

--- postgresql/schema.sql
+++ postgresql/schema.sql
@@ -1,17 +1,18 @@
 CREATE TABLE customer (
-	customer_id varchar(15) PRIMARY KEY,
+	customer_id varchar(50) PRIMARY KEY,
+	zero_star integer DEFAULT 0,
 	one_star integer DEFAULT 0,
 	two_star integer DEFAULT 0,
 	three_star integer DEFAULT 0,
 	four_star integer DEFAULT 0,
 	five_star integer DEFAULT 0,
-	count integer DEFAULT 0,
-	verified_purchase boolean DEFAULT FALSE,
+	total_purchase integer DEFAULT 0,
+	verified_purchase integer DEFAULT 0,
 	helpful_votes integer DEFAULT 0
 );
 
 CREATE TABLE product (
-	product_id varchar(15) PRIMARY KEY,
+	product_id varchar(50) PRIMARY KEY,
 	review_date DATE NOT NULL,
 	avg_rating double precision NOT NULL
 );
