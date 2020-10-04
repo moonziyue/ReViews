c75f8b652598128b533eb068b955d5b23707dc60
moonziyue <moonlesswylb@gmail.com>
2020-10-04 12:04:33 -0400

    Update process_data.py
    aggregate customer avoid pivot()

--- data-processing/process_data.py
+++ data-processing/process_data.py
@@ -1,32 +1,36 @@
 from pyspark.sql.functions import *
 
 def aggregate_customer(df):
-	# agg star_rating
-	customer_rating_df=df.groupby('customer_id').pivot('star_rating').count().fillna(0)
 
-	customer_rating_df=customer_rating_df.withColumnRenamed('1','one_star')
-	customer_rating_df=customer_rating_df.withColumnRenamed('2','two_star')
-	customer_rating_df=customer_rating_df.withColumnRenamed('3','three_star')
-	customer_rating_df=customer_rating_df.withColumnRenamed('4','four_star')
-	customer_rating_df=customer_rating_df.withColumnRenamed('5','five_star')
-	# customer_rating_df.show()
+	df=df.select('customer_id','star_rating','verified_purchase','helpful_votes')
+	
+	customer_summary_df=df.groupBy('customer_id').agg(
+		sum(when(col('star_rating') == 1, 1).otherwise(0)).alias('one_star'),
+		sum(when(col('star_rating') == 2, 1).otherwise(0)).alias('two_star'),
+		sum(when(col('star_rating') == 3, 1).otherwise(0)).alias('three_star'),
+		sum(when(col('star_rating') == 4, 1).otherwise(0)).alias('four_star'),
+		sum(when(col('star_rating') == 5, 1).otherwise(0)).alias('five_star'),
+		count('star_rating').alias('total_purchase'),
+		sum(when(col('verified_purchase') == True, 1).otherwise(0)).alias('verified_purchase'),
+		sum('helpful_votes').alias('helpful_votes')
+    )
 
-	# agg other info
-	stats_df=df.groupBy('customer_id').agg(
-    count('review_id').alias('count'),
-    sum(when(col('verified_purchase') == True, 1)).alias('verified_purchase'),
-    sum('helpful_votes').alias('helpful_votes')
-	)
-	# stats_df.show()
-
-	# join
-	customer_summary_df=customer_rating_df.join(stats_df, 'customer_id', 'inner')
-	# customer_summary_df.show()
+	customer_summary_df=customer_summary_df.withColumn('one_star', customer_summary_df.one_star.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('two_star', customer_summary_df.two_star.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('three_star', customer_summary_df.three_star.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('four_star', customer_summary_df.four_star.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('five_star', customer_summary_df.five_star.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('total_purchase', customer_summary_df.total_purchase.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('verified_purchase', customer_summary_df.verified_purchase.cast('INT'))
+	customer_summary_df=customer_summary_df.withColumn('helpful_votes', customer_summary_df.helpful_votes.cast('INT'))
 
 	return customer_summary_df
 
 def aggregate_product(df):
-	product_summary_df=df.groupby('customer_id','review_date').agg(
+	
+	df=df.select('product_id','review_date','star_rating')
+	
+	product_summary_df=df.groupby('product_id','review_date').agg(
 		avg('star_rating').alias('avg_rating'),
 		count('star_rating').alias('count')
 		)
