00e8f28a5f2ca927b408100fd4bb272d14b087ff
moonziyue <moonlesswylb@gmail.com>
2020-10-11 15:40:43 -0400

    Update config.py

--- dash/config.py
+++ dash/config.py
@@ -10,9 +10,11 @@ class Config:
     """Flask configuration variables."""
 
     # General Config
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
-    SECRET_KEY = environ.get('SECRET_KEY')
+    # FLASK_APP = environ.get('FLASK_APP')
+    # FLASK_ENV = environ.get('FLASK_ENV')
+    # SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = wsgi.py
+    FLASK_ENV = production
 
     # Assets
     LESS_BIN = environ.get('LESS_BIN')
